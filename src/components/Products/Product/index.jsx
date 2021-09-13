ec97c02cc93643385966ed2dd0006f8806b6919a
James Jordan <jpjordan1987@gmail.com>
2021-09-13 15:07:04 -0400

    new format for styling

--- src/components/Products/Product/index.jsx
+++ src/components/Products/Product/index.jsx
@@ -2,15 +2,34 @@ import React from 'react'
 import { Card, CardMedia, CardContent, CardActions, Typography, IconButton} from '@material-ui/core';
 import { AddShoppingCart } from '@material-ui/icons';
 
+import useStyles from './styles'
 
-const Product = () => {
+
+const Product = ({ product }) => {
+
+    const classes = useStyles();
+
     return (
+        <main>
 
-        
-
-        <Card>
-
+        <Card className={classes.root} image='' title={product.name}>
+            <CardMedia />
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant='h5' gutterBottom>
+                        {product.price}
+                    </Typography>
+                </div>
+                    <Typography variant='h2' color='textSecondary' >{product.description}</Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label='Add to Cart' >
+                    <AddShoppingCart/>
+                </IconButton>
+            </CardActions>
         </Card>
+
+        </main>
     )
 }
 
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Grid, { GridList } from '@material-ui/core';
+import Product from './Product'
 
 const myproducts=[
-    {id: 1, name:'writing-1', description:'gospel-tract'},
-    {id: 2, name:'writing-2', description:'the-vows-are-vague'},
+    {id: 1, name:'writing-1', description:'gospel-tract', price:.25},
+    {id: 2, name:'writing-2', description:'the-vows-are-vague', price:.25},
 ]
 const Products = () => {
 
@@ -12,7 +13,7 @@ const Products = () => {
     <Grid container justify = 'center' spacing = {4}> 
         {myproducts.map((product)=>(
         <Grid item key ={product.id} xs={12} sm={6} md={4} lg={3}>
-            <Product /> 
+            <Product product={product}/> 
             
         </Grid>
         ))}
